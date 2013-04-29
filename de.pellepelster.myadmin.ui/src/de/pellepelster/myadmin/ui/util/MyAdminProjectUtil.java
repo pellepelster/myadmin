08701e330674debb084dec5db015910499a33726
Christian Pelster <pellepelster@gmail.com>
2013-04-29 22:07:31 +0200

    project ui utils

---
+++ de.pellepelster.myadmin.ui/src/de/pellepelster/myadmin/ui/util/MyAdminProjectUtil.java
@@ -0,0 +1,7 @@
+package de.pellepelster.myadmin.ui.util;
+
+public class MyAdminProjectUtil
+{
+	public static final String FQDN_PROJECT_NAME_REGEX = "([a-zA-Z_][a-zA-Z\\d_]*\\.)+[A-Z][a-zA-Z\\d_]*";
+
+}
