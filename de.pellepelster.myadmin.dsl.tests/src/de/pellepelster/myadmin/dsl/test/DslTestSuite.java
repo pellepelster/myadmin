7b510e1fd0f46399da1d5ed1d4ca4378228eb4a5
Christian Pelster <pellepelster@gmail.com>
2013-05-06 20:19:46 +0200

    removed empty test

--- de.pellepelster.myadmin.dsl.tests/src/de/pellepelster/myadmin/dsl/test/DslTestSuite.java
+++ de.pellepelster.myadmin.dsl.tests/src/de/pellepelster/myadmin/dsl/test/DslTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EntityQueryTest.class, ModelQueryTest.class, ModelUtilTest.class })
+@SuiteClasses({ EntityQueryTest.class, ModelQueryTest.class })
 public class DslTestSuite
 {
 
