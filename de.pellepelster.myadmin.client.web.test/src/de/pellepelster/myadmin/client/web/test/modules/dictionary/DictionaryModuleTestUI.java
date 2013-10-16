117db61463165ed626952671413f768962c3a78d
Pelle Pelster <pelle@pelle.io>
2013-10-16 18:14:13 +0200

    build file cleanup

---
+++ de.pellepelster.myadmin.client.web.test/src/de/pellepelster/myadmin/client/web/test/modules/dictionary/DictionaryModuleTestUI.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2013 Christian Pelster.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Christian Pelster - initial API and implementation
+ */
+package de.pellepelster.myadmin.client.web.test.modules.dictionary;
+
+import de.pellepelster.myadmin.client.base.layout.IModuleUI;
+import de.pellepelster.myadmin.client.web.modules.navigation.ModuleNavigationModule;
+
+/**
+ * UI for the navigation module
+ * 
+ * @author pelle
+ * 
+ */
+public class DictionaryModuleTestUI implements IModuleUI
+{
+	private ModuleNavigationModule module;
+
+	public DictionaryModuleTestUI(ModuleNavigationModule module)
+	{
+		super();
+		this.module = module;
+	}
+
+	@Override
+	public boolean close()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean contributesToBreadCrumbs()
+	{
+		return false;
+	}
+
+	@Override
+	public Object getContainer()
+	{
+		return null;
+	}
+
+	@Override
+	public Object getModule()
+	{
+		return null;
+	}
+
+	@Override
+	public String getTitle()
+	{
+		return null;
+	}
+
+}
