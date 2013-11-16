e6461af2c82eb466cd8c8405c46607cebabe5247
Pelle Pelster <pelle@pelle.io>
2013-11-16 15:59:57 +0100

    demo build fix

--- de.pellepelster.myadmin.demo.client/src/de/pellepelster/myadmin/demo/client/TestClientHierarchicalConfiguration.java
+++ de.pellepelster.myadmin.demo.client/src/de/pellepelster/myadmin/demo/client/TestClientHierarchicalConfiguration.java
@@ -1,7 +1,6 @@
 package de.pellepelster.myadmin.demo.client;
 
 import de.pellepelster.myadmin.client.base.modules.hierarchical.BaseHierarchicalConfiguration;
-import de.pellepelster.myadmin.demo.client.web.DemoDictionaryIDs;
 
 public class TestClientHierarchicalConfiguration extends BaseHierarchicalConfiguration
 {
@@ -11,8 +10,9 @@ public class TestClientHierarchicalConfiguration extends BaseHierarchicalConfigu
 	{
 		super(ID);
 
-		addHierarchy(DemoDictionaryIDs.COMPANY);
-		addHierarchy(DemoDictionaryIDs.MANAGER, DemoDictionaryIDs.COMPANY);
-		addHierarchy(DemoDictionaryIDs.EMPLOYEE, DemoDictionaryIDs.COMPANY, DemoDictionaryIDs.MANAGER);
+		// addHierarchy(DemoDictionaryIDs.COMPANY);
+		// addHierarchy(DemoDictionaryIDs.MANAGER, DemoDictionaryIDs.COMPANY);
+		// addHierarchy(DemoDictionaryIDs.EMPLOYEE, DemoDictionaryIDs.COMPANY,
+		// DemoDictionaryIDs.MANAGER);
 	}
 }
